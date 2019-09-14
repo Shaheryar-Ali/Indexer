fc07b0c236abf9e6da45f41fea29ff63f682ea3d
Shaheryar-Ali <alishaheryarsonic@gmail.com>
2019-09-14 21:22:59 +0500

    tokenize + docIDs + termIDs

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os.path
 from nltk.stem import PorterStemmer
 from lxml import html
 
+
 class Token:
     directory = ""
     __termId = 0
@@ -14,11 +15,11 @@ class Token:
     termDictionary = {}
     documentDictionary = {}
 
-    #We assign the directory to the class that was inputted through console
+    # We assign the directory to the class that was inputted through console
     def __init__(self, path):
         self.directory = path
 
-    #Gets a list of all path in directory
+    # Gets a list of all path in directory
     def ListofFiles(self):
         FilesList = []
         for file in glob.glob(self.directory):
@@ -28,53 +29,74 @@ class Token:
     def ListofStopWords(self):
         file = open("stoplist.txt", "r")
         stopWords = file.read()
+        file.close()
         return stopWords
 
     def GetText(self, path):
-        soup = BeautifulSoup(open(path, encoding="utf-8", errors="surrogateescape"), 'html5lib')
-        for tags in soup(['script', 'style']):
+        file = open(path, encoding="utf-8", errors="surrogateescape")
+        content = file.read()
+        content = content[content.index('<'):]
+        soup = BeautifulSoup(content, 'html5lib')
+        body = soup.find("body")
+        for tags in body(['script', 'style']):
             tags.decompose()
         try:
-            text = soup.html.body.get_text()
-        except AttributeError as e:          #in case we fail to read the file
+            text = body.get_text()
+        except AttributeError as e:  # in case we fail to read the file
             return ':'
         lines = (line.strip() for line in text.splitlines())
         chunks = (phrase.strip() for line in lines for phrase in line.split(" "))
         text = '\n'.join(chunk for chunk in chunks if chunk)
+        file.close()
         return text
+
+    def StoreTermId(self):
+        file = open("termids.txt", "w+")
+        for term in self.termDictionary:
+            file.write(str(self.termDictionary[term]) + "\t" + str(term) + "\n")
+        file.close()
+
+    def StoreDocId(self):
+        file = open("docids.txt", "w+")
+        for doc in self.documentDictionary:
+            file.write(str(self.documentDictionary[doc]) + "\t" + str(doc) + "\n")
+        file.close()
 
     def Tokenize(self):
         files = self.ListofFiles()
         stopWordList = self.ListofStopWords()
         for file in files:
-            self.documentDictionary[os.path.basename(file)] = self.__docId; #Gives DocId to document
-            self.__docId = self.__docId+1
+            self.documentDictionary[os.path.basename(file)] = self.__docId;  # Gives DocId to document
+            self.__docId = self.__docId + 1
             text = self.GetText(file)
             if text == ':':
-                print("Failed to read DocID: ", self.__docId-1)
+                print("Failed to read DocID: ", self.__docId - 1)
                 continue
-            tokenV1 = re.split("[^a-zA-Z0-9]", text)
-            tokenV2 = [token.lower() for token in tokenV1]
-            tokenV1.clear()
-            #Removing stopwords
-            tokenV3 = []
-            for token in tokenV2:
+            tokenv1 = re.split("[^a-zA-Z0-9]", text)
+            tokenv2 = [token.lower() for token in tokenv1]
+            tokenv1.clear()
+            # Removing stopwords
+            tokenv3 = []
+            for token in tokenv2:
                 if token not in stopWordList:
-                    tokenV3.append(token)
-            tokenV2.clear()
-            #porter Stemming the terms
-            tokenV4 = []
+                    tokenv3.append(token)
+            tokenv2.clear()
+            # porter Stemming the terms
+            tokenv4 = []
             porter = PorterStemmer()
-            for token in tokenV3:
-                tokenV4.append(porter.stem(token))
-            tokenV3.clear()
+            for token in tokenv3:
+                tokenv4.append(porter.stem(token))
+            tokenv3.clear()
+            for term in tokenv4:
+                if (not self.termDictionary.__contains__(term)):
+                    self.termDictionary[term] = self.__termId;
+                    self.__termId = self.__termId + 1;
+            tokenv4.clear()
+        self.StoreTermId()
+        self.StoreDocId()
 
 
-
-
-
-
-
+# Class Token ends here
 
 
 def main(arg):
@@ -83,4 +105,3 @@ def main(arg):
 
 
 main("corpus\*")
-
