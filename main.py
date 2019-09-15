b37dec9bc5da6b7e25ec482816138d37f3d21366
Shaheryar-Ali <alishaheryarsonic@gmail.com>
2019-09-15 05:02:10 +0500

    Inverted Index without Hash

--- main.py
+++ main.py
@@ -62,6 +62,24 @@ class Token:
             file.write(str(self.documentDictionary[doc]) + "\t" + str(doc) + "\n")
         file.close()
 
+    def ReduceToken(self, text, stopWordList):
+        tokenv1 = re.split("[^a-zA-Z]", text)
+        tokenv2 = [token.lower() for token in tokenv1]
+        tokenv1.clear()
+        # Removing stopwords
+        tokenv3 = []
+        for token in tokenv2:
+            if token not in stopWordList:
+                tokenv3.append(token)
+        tokenv2.clear()
+        # porter Stemming the terms
+        tokenv4 = []
+        porter = PorterStemmer()
+        for token in tokenv3:
+            tokenv4.append(porter.stem(token))
+        tokenv3.clear()
+        return tokenv4
+
     def Tokenize(self):
         files = self.ListofFiles()
         stopWordList = self.ListofStopWords()
@@ -72,36 +90,103 @@ class Token:
             if text == ':':
                 print("Failed to read DocID: ", self.__docId - 1)
                 continue
-            tokenv1 = re.split("[^a-zA-Z0-9]", text)
-            tokenv2 = [token.lower() for token in tokenv1]
-            tokenv1.clear()
-            # Removing stopwords
-            tokenv3 = []
-            for token in tokenv2:
-                if token not in stopWordList:
-                    tokenv3.append(token)
-            tokenv2.clear()
-            # porter Stemming the terms
-            tokenv4 = []
-            porter = PorterStemmer()
-            for token in tokenv3:
-                tokenv4.append(porter.stem(token))
-            tokenv3.clear()
-            for term in tokenv4:
-                if (not self.termDictionary.__contains__(term)):
-                    self.termDictionary[term] = self.__termId;
-                    self.__termId = self.__termId + 1;
-            tokenv4.clear()
+            stemmedtext = self.ReduceToken(text, stopWordList)
+            for term in stemmedtext:
+                if not self.termDictionary.__contains__(term):
+                    self.termDictionary[term] = self.__termId
+                    self.__termId = self.__termId + 1
+            stemmedtext.clear()
         self.StoreTermId()
         self.StoreDocId()
 
-
 # Class Token ends here
+
+class InvertedIndexNoHash:
+    Dictionary = {}
+    termFrequencyList = {}
+    docFrequencyList = {}
+    termDict = {}
+    docDict = {}
+    token = object
+
+    def __init__(self, token):
+        self.token = token
+        self.termDict = token.termDictionary
+        self.docDict = token.documentDictionary
+        self.InitializeFrequencyList()
+
+    def InitializeFrequencyList(self):
+        for term in self.termDict:
+            self.termFrequencyList[term] = 0
+            self.docFrequencyList[term] = 0
+            self.Dictionary[term] = []
+
+    def MakeList(self):
+        FilesList = self.token.ListofFiles()
+        Stopwords = self.token.ListofStopWords()
+        for file in FilesList:
+            text = self.token.GetText(file)
+            if text == ':':
+                print("Failed to open")
+                continue
+            stemmedtext = self.token.ReduceToken(text, Stopwords)
+            filename = os.path.basename(file)
+            docID = self.docDict[filename]
+            position = 1
+            WordinDoc = []
+            for word in stemmedtext:
+                self.termFrequencyList[word] += 1
+                if word not in WordinDoc:
+                    WordinDoc.append(word)
+                    self.docFrequencyList[word] += 1
+                self.Dictionary[word].append((docID, position))
+                position += 1
+            WordinDoc.clear()
+
+    def SavetoFile(self):
+        file = open("term_index.txt", "w+")
+        for term in self.termDict:
+            file.write(str(self.termDict[term]) + " " + str(self.termFrequencyList[term]) + " "  + str(self.docFrequencyList[term]))
+            previous_doc = 0
+            previous_position = 0
+            for docId,position in self.Dictionary[term]:
+                if docId == previous_doc:
+                    file.write(" " + str(docId - previous_doc) + "," + str(position - previous_position))
+                    previous_position = position
+                else:
+                    file.write(" " + str(docId - previous_doc) + "," + str(position))
+                    previous_position = position
+                    previous_doc = docId
+            file.write('\n')
+        file.close()
+
+
+ #End of class InvertedIndexNoHash
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main(arg):
     token = Token(arg)
     token.Tokenize()
+    NoHash = InvertedIndexNoHash(token)
+    NoHash.MakeList()
+    NoHash.SavetoFile()
 
 
 main("corpus\*")
---
+++ read_index.py
@@ -0,0 +1,7 @@
+
+def main(argv):
+    file = open("term_index","r")
+    list = file.read()
+
+
+main("cancer")
