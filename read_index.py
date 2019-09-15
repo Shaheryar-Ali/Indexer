53ea110821937b3bb756838017a134e2b2392b9c
Shaheryar-Ali <alishaheryarsonic@gmail.com>
2019-09-15 13:33:21 +0500

    read index

--- read_index.py
+++ read_index.py
@@ -1,7 +1,26 @@
 
-def main(argv):
-    file = open("term_index","r")
-    list = file.read()
+def read_index(argv):
+    file1 = open("termids.txt","r")
+    list = file1.read()
+    index = -1
 
+    for word in list.split():
+        if(word == argv):
+            break
+        index = word
 
-main("cancer")
+    id = int(index)
+
+    file2 = open("term_index.txt","r")
+    lines = file2.readlines()
+
+    info = []
+    for value in lines[id].split(' '):
+        info.append(value)
+
+    print("Listing for term: ", argv)
+    print("TERMID: ", id)
+    print("Number of documents containing term:", info[2] )
+    print("Term frequency in corpus: ", info[1])
+
+read_index("age")
